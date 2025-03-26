fa4992a46da40da237152aa0b69d48e7857f542f
Radheycinema <radheshyamjakhar175@gmail.com>
2025-03-26 22:00:30 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -51,8 +51,8 @@ async def start(client, message):
             buttons = [[
                 InlineKeyboardButton('⤬ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-                InlineKeyboardButton('ᴇᴀʀɴ ᴍᴏɴᴇʏ', callback_data="shortlink_info"),
-                InlineKeyboardButton('ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ', url=GRP_LNK)
+                InlineKeyboardButton('SUPPORT GROUP', url='https://t.me/+iTdaDXPgqGhhNWQ1'),
+                InlineKeyboardButton('MOVIE GROUP', url='https://t.me/+hItR7_lFaKxmMjU1')
             ],[
                 InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
                 InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
