d9949f91d6751096b030b4a382802caa38d1ee05
Radheycinema <radheshyamjakhar175@gmail.com>
2025-03-27 00:13:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -53,7 +53,7 @@ async def start(client, message):
             ],[
                 InlineKeyboardButton('SUPPORT GROUP', url='https://t.me/+iTdaDXPgqGhhNWQ1'),
                 InlineKeyboardButton('MOVIE GROUP', url='https://t.me/+hItR7_lFaKxmMjU1')
-            ],,[
+            ],[
                 InlineKeyboardButton('ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/rajmoviesword')
             ]]
         else:
