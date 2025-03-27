a5194583fefec344bcf43146ba87206d37d101b1
Radheycinema <radheshyamjakhar175@gmail.com>
2025-03-27 21:55:11 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -47,7 +47,7 @@ async def start(client, message):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
-        if PREMIUM_AND_REFERAL_MODE == False:
+        if PREMIUM_AND_REFERAL_MODE ==  tmTrue:
             buttons = [[
                 InlineKeyboardButton('⤬ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ]]
