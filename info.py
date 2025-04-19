531455c0186b2cfef9eaf13b8a1df8183e043df8
Radheycinema <radheshyamjakhar175@gmail.com>
2025-04-19 16:43:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,14 +88,14 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+hItR7_lFaKxmMjU1')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+ypN2t5h6j_plMGI9')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/rajmoviesword')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+iTdaDXPgqGhhNWQ1') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+qZPlJvZXj3k5ZGVl') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/RJRAAJ55')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
-PM_SEARCH = bool(environ.get('PM_SEARCH', False))
+PM_SEARCH = bool(environ.get('PM_SEARCH', True))
 BUTTON_MODE = bool(environ.get('BUTTON_MODE', True))
 MAX_BTN = bool(environ.get('MAX_BTN', True))
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
